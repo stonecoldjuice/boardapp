c84dd72569557f17f37a57cd5dba49c29b4a1549
stonecoldjuice <singingmoi@gmail.com>
2017-04-21 10:26:18 +0900

    User Login

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var mongoose = require('mongoose');
+var passport = require('passport');
+var session = require('express-session');
+var flash = require('connect-flash');
+var async = require('async');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var LocalStrategy = require('passport-local').Strategy;
 
 mongoose.connect(process.env.MONGO_DB);
 
@@ -13,96 +20,242 @@ db.on("error", function(err) {
     console.log("DB ERROR :", err);
 });
 
-var dataSchema = mongoose.Schema({
-    name: String,
-    count: Number
+var postSchema = mongoose.Schema({
+    title: {type: String, required: true},
+    body: {type: String, required: true},
+    createdAt: {type: Date, default: Date.now},
+    updatedAt: Date
 });
+var Post = mongoose.model('post', postSchema);
 
-var Data = mongoose.model('data', dataSchema);
-Data.findOne({name:"myData"}, function(err, data) {
-    if (err) {
-        return console.log("Data ERROR1:", err);
-    }
-
-    if (!data) {
-        Data.create({name: "myData", count: 0}, function(err, data) {
-            if (err) {
-                return console.log("Data ERROR2:", err);
-            }
-
-            console.log("Counter intitialized :", data);
-        });
-    }
+var userSchema = mongoose.Schema({
+    email: {type: String, required: true, unique: true},
+    nickname: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    createdAt: {type: Date, default:Date.now}
 });
+var User = mongoose.model('user', userSchema);
 
 app.set("view engine", 'ejs');
-// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.use(flash());
 
+app.use(session({secret: 'MySecret'}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    })
+});
+passport.use('local-login',
+    new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    },
+    function(req, email, password, done) {
+        User.findOne({'email': email}, function(err, user) {
+            if (err) {
+                return done(err);
+            }
+            if (!user) {
+                req.flash("email", req.body.email);
+                return done(null, false, req.flash('loginError', 'No user found'));
+            }
+            if (user.password != password) {
+                req.flash("email", req.body.email);
+                return done(null, false, req.flash('loginError', 'Password does not Match'));
+            }
+            return done(null, user);
+        })
+    }
+));
 app.get('/', function(req, res) {
-    Data.findOne({name: "myData"}, function(err, data) {
-        if (err) {
-            return console.log("Data ERROR: ", err);
-        }
-
-        data.count++;
-
-        data.save(function(err) {
-            if (err) {
-                return console.log("Data ERROR: ", err);
-            }
-            res.render('my_first_ejs', data);
-        });
+    res.redirect('/posts');
+});
+app.get('/login', function(req, res) {
+    res.render('login/login', {email: req.flash("email")[0], loginError:req.flash('loginError')});
+});
+app.post('/login',
+  function (req,res,next){
+    req.flash("email"); // flush email data
+    if(req.body.email.length === 0 || req.body.password.length === 0){
+      req.flash("email", req.body.email);
+      req.flash("loginError","Please enter both email and password.");
+      res.redirect('/login');
+    } else {
+      next();
+    }
+  }, passport.authenticate('local-login', {
+    successRedirect : '/posts',
+    failureRedirect : '/login',
+    failureFlash : true
+  })
+);
+app.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+app.get('/users/new', function(req, res) {
+    res.render('users/new', {
+        formData: req.flash('formData')[0],
+        emailError: req.flash('emailError')[0],
+        nicknameError: req.flash('nicknameError')[0],
+        passwordError: req.flash('passwordError')[0]
     });
 });
-
-app.get('/reset', function(req, res) {
-    setCounter(res, 0);
+app.post('/users', checkUserRegValidation, function(req, res, next) {
+    User.create(req.body.user, function(err, user) {
+        if (err) {
+            return res.json({success: false, message: err});
+        }
+        res.redirect('/login');
+    });
 });
-
-app.get('/set/count', function(req, res) {
-    if (req.query.count) {
-        setCounter(res, req.query.count);
-    } else {
-        getCounter(res);
-    }
-})
-
-app.get('/set/:num', function(req, res) {
-    if (req.params.num) {
-        setCounter(res, req.params.num);
-    } else {
-        getCounter(res);
-    }
-})
-
-function setCounter(res, num) {
-    console.log("setCounter");
-    Data.findOne({name: "myData"}, function(err, data) {
+app.get('/users/:id', function(req, res) {
+    User.findById(req.params.id, function(err, user) {
         if (err) {
-            return console.log("Data ERROR: ", err);
+            return res.json({success: false, message: err});
         }
-
-        data.count = num;
-
-        data.save(function(err) {
-            if (err) {
-                return console.log("Data ERROR", err);
+        res.render("users/show", {user: user});
+    });
+});
+app.get('/usesrs/:id/edit', function(req, res) {
+    User.findById(req.params.id, function(err, user) {
+        if (err) {
+            return res.json({success: false, message: err}); 
+        }
+        res.render("users/edit", {
+                user: user,
+                formData: req.flash('formData')[0],
+                emailError: req.flash('emailError')[0],
+                nicknameError: req.flash('nicknameError')[0],
+                passwordError: req.flash('passwordError')[0]
+            });
+    });
+});
+app.put('/users/:id', checkUserRegValidation, function(req, res) {
+    User.findById(req.params.id, req.body.user, function(err, user) {
+        if (err) {
+            return res.json({success: "false", message: err});
+        }
+        if (req.body.user.password == user.password) {
+            if (req.body.user.newPassword) {
+                req.body.user.password = req.body.user.newPassword;
+            } else {
+                delete req.body.user.password;
             }
-            res.render('my_first_ejs', data);
-        });
-    });
-}
 
-function getCounter(res) {
-    console.log("getCounter");
-    Data.findOne({name: "myData"}, function(err, data) {
-        if (err) {
-            return console.log("Data ERROR: ", err);
+            User.findByIdAndUpdate(req.params.id, req.body.user, function(err, user) {
+                if (err) {
+                    return res.json({success: "false", message: err});
+                }
+                res.redirect('/users/' + req.params.id);
+            });
+        } else {
+            req.flash("formData", req.body.user);
+            req.flash("passwordError", "- Invalid password");
+            res.redirect('/users/' + req.params.id + "/edit");
         }
-        res.render('my_first_ejs', data);
     });
-}
-
+});
+app.get('/posts', function(req, res) {
+    Post.find({}).sort('-createdAt').exec(function(err, posts) {
+        if (err) {
+            return res.json({success: false, message: err});
+        }
+        res.render("posts/index", {data: posts, user: req.user});
+    });
+});
+app.get('/posts/new', function(req, res) {
+    res.render("posts/new");
+});
+app.post('/posts', function(req, res) {
+    Post.create(req.body.post, function(err, post) {
+        if (err) {
+            return res.json({success: false, message: err});
+        }
+        res.redirect('/posts');
+    })
+})
+app.get('/posts/:id', function(req, res) {
+    Post.findById(req.params.id, function(err, post) {
+        if (err) {
+            res.json({success: false, message: err});
+        }
+        res.render("posts/show", {data: post});
+    });
+});
+app.get('/posts/:id/edit', function(req, res) {
+    Post.findById(req.params.id, function(err, post) {
+        if (err) {
+            return res.json({success: false, message: err});
+        }
+        res.render("posts/edit", {data: post});
+    })
+})
+app.put('/posts/:id', function(req, res) {
+    req.body.post.updatedAt=Date.now();
+    Post.findByIdAndUpdate(req.params.id, req.body.post, function(err, post) {
+        if (err) {
+            return res.json({success: false, message: err});
+        }
+        res.redirect('/posts/' + req.params.id);
+    });
+});
+app.delete('/posts/:id', function(req, res) {
+    Post.findByIdAndRemove(req.params.id, function(err, post) {
+        if (err) {
+            return res.json({success: false, message: err});
+        }
+        res.redirect('/posts');
+    });
+});
 app.listen(3000, function() {
     console.log('Server On!');
 });
+
+function checkUserRegValidation(req, res, next) {
+    var isValid = true;
+
+    async.waterfall(
+        [ function(callback) {
+            User.findOne({email: req.body.user.email, _id: {$ne: mongoose.Types.ObjectId(req.params.id)}},
+            function(err, user) {
+                if (user) {
+                    isValid = false;
+                    req.flash("emailError", "- This email is already resistered.");
+                }
+                callback(null, isValid);
+            });
+        },
+        function(isValid, callback) {
+            User.findOne({nickname: req.body.user.nickname, _id: {$ne: mongoose.Types.ObjectId(req.params.id)}},
+            function(err, user) {
+                if (user) {
+                    isValid = false;
+                    req.flash("nicknameError", "- This nickname is already resistered.");
+                }
+                callback(null, isValid);
+            });
+        }], function(err, isvalid) {
+            if (err) {
+                return res.json({success: "false", message: err});
+            }
+            if (isValid) {
+                return next();
+            } else {
+                req.flash("formData", req.body.user);
+                res.redirect("back");
+            }
+        }
+    )
+}
